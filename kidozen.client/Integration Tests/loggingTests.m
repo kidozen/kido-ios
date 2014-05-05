5784b0a8051e2fda726112954091f5735ff0c83d
Nicolas Miyasato <nmiyasato@gmail.com>
2014-05-05 09:17:10 -0300

    Adding separate logging tests file, instead of messing around the datasources one.

---
+++ kidozen.client/Integration Tests/loggingTests.m
@@ -0,0 +1,77 @@
+//
+//  loggingTests.m
+//  kidozen.client
+//
+//  Created by Nicolas Miyasato on 5/5/14.
+//  Copyright (c) 2014 Tellago Studios. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "Constants.h"
+#import "KZApplication.h"
+
+@interface loggingTests : XCTestCase
+
+@property (nonatomic, strong) KZApplication *application;
+@property (nonatomic, strong) NSDictionary *nestedDataDict;
+@property (nonatomic, strong) NSDictionary *dataDict;
+
+@end
+
+@implementation loggingTests
+
+- (void)setUp
+{
+    [super setUp];
+    self.nestedDataDict = @{@"path": @"/",
+                            @"qa" : @{@"k": @"kidozen"}
+                            };
+    
+    self.dataDict = @{@"path": @"?k=kidozen"};
+    // Put setup code here; it will be run once, before the first test case.
+    if (!self.application) {
+        dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
+
+        self.application = [[KZApplication alloc] initWithTennantMarketPlace:kzAppCenterUrl
+                                                             applicationName:kzAppName
+                                                              applicationKey:@"TG2wIc9xnCsZmcYaiC/+g1FpAP96X+G0ZKXjCzH/viM="
+                                                                   strictSSL:NO
+                                                                 andCallback:^(KZResponse * r) {
+                                                                     XCTAssertNotNil(r.response,@"Invalid response");
+                                                                     NSLog(@"%@", r);
+                                                                     dispatch_semaphore_signal(semaphore);
+                                                                 }];
+        
+        
+        
+        
+        assert(self.application);
+        while (dispatch_semaphore_wait(semaphore, DISPATCH_TIME_NOW))
+            [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate dateWithTimeIntervalSinceNow:100]];
+    }
+}
+
+- (void)tearDown
+{
+    [super tearDown];
+}
+
+- (void)testShouldExecuteLog
+{
+    dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
+    
+    [self.application writeLog:@{@"key" : @"value"}
+                     withLevel:LogLevelVerbose completion:^(KZResponse *r) {
+                         
+                         XCTAssertNotNil(r,@"invalid response");
+                         XCTAssertEqual(201, r.urlResponse.statusCode, @"Invalid status code");
+                         dispatch_semaphore_signal(semaphore);
+                         
+                     }];
+    
+    while (dispatch_semaphore_wait(semaphore, DISPATCH_TIME_NOW))
+        [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate dateWithTimeIntervalSinceNow:100]];
+}
+
+
+@end
