a9ef83d712378211baa21dba97896433195b8c35
Nicolas Miyasato <nmiyasato@gmail.com>
2014-05-05 10:13:00 -0300

    Adding test for override of applicationKey token with username and password

--- kidozen.client/Integration Tests/loggingTests.m
+++ kidozen.client/Integration Tests/loggingTests.m
@@ -15,8 +15,6 @@ static NSString *const kApplicationKey = @"TG2wIc9xnCsZmcYaiC/+g1FpAP96X+G0ZKXjC
 @interface loggingTests : XCTestCase
 
 @property (nonatomic, strong) KZApplication *application;
-@property (nonatomic, strong) NSDictionary *nestedDataDict;
-@property (nonatomic, strong) NSDictionary *dataDict;
 
 @end
 
@@ -25,12 +23,7 @@ static NSString *const kApplicationKey = @"TG2wIc9xnCsZmcYaiC/+g1FpAP96X+G0ZKXjC
 - (void)setUp
 {
     [super setUp];
-    self.nestedDataDict = @{@"path": @"/",
-                            @"qa" : @{@"k": @"kidozen"}
-                            };
     
-    self.dataDict = @{@"path": @"?k=kidozen"};
-    // Put setup code here; it will be run once, before the first test case.
     if (!self.application) {
         dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
 
@@ -40,7 +33,6 @@ static NSString *const kApplicationKey = @"TG2wIc9xnCsZmcYaiC/+g1FpAP96X+G0ZKXjC
                                                                    strictSSL:NO
                                                                  andCallback:^(KZResponse * r) {
                                                                      XCTAssertNotNil(r.response,@"Invalid response");
-                                                                     NSLog(@"%@", r);
                                                                      dispatch_semaphore_signal(semaphore);
                                                                  }];
         
@@ -75,5 +67,47 @@ static NSString *const kApplicationKey = @"TG2wIc9xnCsZmcYaiC/+g1FpAP96X+G0ZKXjC
         [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate dateWithTimeIntervalSinceNow:100]];
 }
 
+- (void)testShouldOverrideApplicationKeyWithUsernameAndPassword
+{
+    
+    dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
+    self.application = [[KZApplication alloc] initWithTennantMarketPlace:kzAppCenterUrl
+                                                         applicationName:kzAppName
+                                                               strictSSL:NO
+                                                             andCallback:^(KZResponse * r) {
+                                                                 XCTAssertNotNil(r.response,@"Invalid response");
+                                                                 [r.application authenticateUser:kzUser
+                                                                                    withProvider:kzProvider
+                                                                                     andPassword:kzPassword
+                                                                                      completion:^(id c) {
+                                                                                          XCTAssertNotNil(c,@"User not authenticated");
+                                                                                          dispatch_semaphore_signal(semaphore);
+                                                                 }];
+                                                             }];
+    while (dispatch_semaphore_wait(semaphore, DISPATCH_TIME_NOW))
+        [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate dateWithTimeIntervalSinceNow:100]];
+    
+    
+    // If user authenticated with username and password, one way to test whether it successfully
+    // overrode the applicationKey token, is to try to call a datasource which only has permissions
+    // to run using username and password.
+    [self executeDataSource];
+    
+}
+
+- (void) executeDataSource
+{
+    
+    KZDatasource *ds = [self.application DataSourceWithName:@"test-query"];
+    dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
+    [ds Query:^(KZResponse *r) {
+        XCTAssertNotNil(r,@"invalid response");
+        NSNumber* status =  [[r.response objectForKey:@"data"] objectForKey:@"status"] ;
+        XCTAssertEqual(200,[status intValue], @"Invalid status code");
+        dispatch_semaphore_signal(semaphore);
+    }];
+    while (dispatch_semaphore_wait(semaphore, DISPATCH_TIME_NOW))
+        [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate dateWithTimeIntervalSinceNow:100]];
+}
 
 @end
