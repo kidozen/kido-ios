61b58093c9bedc1112d922493b17658561567883
Nicolas Miyasato <nmiyasato@gmail.com>
2014-05-05 17:47:49 -0300

    Adding test for timeout

--- kidozen.client/Integration Tests/loggingTests.m
+++ kidozen.client/Integration Tests/loggingTests.m
@@ -112,16 +112,16 @@ static NSString *const kApplicationKey = @"TG2wIc9xnCsZmcYaiC/+g1FpAP96X+G0ZKXjC
 
 
 // I cannot seem to make this work due to not being able to add the CURRENTLY_TESTING macro
-//-(void) testShouldTimeOutAndRecoverKey
-//{
-//    dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
-//    self.application.authCompletionBlock = ^(id response) {
-//        NSLog(@"%@", response);
-//        dispatch_semaphore_signal(semaphore);
-//
-//    };
-//    while (dispatch_semaphore_wait(semaphore, DISPATCH_TIME_NOW))
-//        [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate dateWithTimeIntervalSinceNow:100]];
-//}
+-(void) testShouldTimeOutAndRecoverKey
+{
+    dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
+    self.application.authCompletionBlock = ^(id response) {
+        NSLog(@"%@", response);
+        dispatch_semaphore_signal(semaphore);
+
+    };
+    while (dispatch_semaphore_wait(semaphore, DISPATCH_TIME_NOW))
+        [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate dateWithTimeIntervalSinceNow:100]];
+}
 
 @end
