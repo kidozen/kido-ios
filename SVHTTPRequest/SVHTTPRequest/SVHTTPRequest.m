c5fb1eb92a21e7a17c584b5bf5b7b98880f24613
Nicolas Miyasato <nmiyasato@gmail.com>
2014-05-13 11:17:59 -0300

    Removing some logs.

--- SVHTTPRequest/SVHTTPRequest/SVHTTPRequest.m
+++ SVHTTPRequest/SVHTTPRequest/SVHTTPRequest.m
@@ -227,8 +227,6 @@ static NSTimeInterval SVHTTPRequestTimeoutInterval = 100;
                         NSError *jsonError;
                         NSData *jsonData = [NSJSONSerialization dataWithJSONObject:parameters options:0 error:&jsonError];
                         [self.operationRequest setHTTPBody:jsonData];
-                    NSLog(@"data is %@", self.operationRequest.HTTPBody);
-                    NSLog(@"Headers are %@", [self.operationRequest allHTTPHeaderFields]);
                 }
                 else if ([parameters isKindOfClass:[NSString class]] ||
                          [parameters isKindOfClass:[NSNumber class]]) {
