f5821924af080dc84c0cd5bea502612b05b798d1
Nicolas Miyasato <nmiyasato@gmail.com>
2014-05-27 10:31:06 -0300

    Little refactor on kzmail service

--- kidozen.client/kidozen.client/KZMail.m
+++ kidozen.client/kidozen.client/KZMail.m
@@ -10,43 +10,51 @@
 
 -(void) send:(NSDictionary *)email attachments:(NSDictionary*)attachments completion:(void (^)(KZResponse *))block
 {
+    
+    __weak KZMail *safeMe = self;
+    
     if (attachments != nil) {
         _client.sendParametersAsJSON = NO;
-        [_client setHeaders:@{@"Authorization": self.kzToken,
-                              @"Accept" :@"application/json"}];
         
-        [_client POST:@"/attachments" parameters:attachments completion:^(id response, NSHTTPURLResponse *urlResponse, NSError *error) {
-            NSError * restError = nil;
-            if ([urlResponse statusCode]>KZHttpErrorStatusCode) {
-                restError = error;
-            }
-
-            _client.sendParametersAsJSON = YES;
-
-            [_client setHeaders:[NSDictionary dictionaryWithObject:self.kzToken forKey:@"Authorization"]];
-            NSMutableDictionary *mailDictionary = [NSMutableDictionary dictionaryWithDictionary:email];
-            mailDictionary[@"attachments"] = response;
-            [_client POST:@"" parameters:mailDictionary completion:^(id response, NSHTTPURLResponse *urlResponse, NSError *error) {
-                NSError * restError = nil;
-                if ([urlResponse statusCode]>KZHttpErrorStatusCode) {
-                    restError = error;
-                }
-                block( [[KZResponse alloc] initWithResponse:response urlResponse:urlResponse andError:restError] );
-            }];
-            
-        }];
+        [self sendEmailToPath:@"/attachments"
+                   parameters:attachments
+                   completion:^(KZResponse *response) {
+                       
+                       if ([response.urlResponse statusCode]>KZHttpErrorStatusCode)
+                       {
+                           block(response);
+                       }
+                       else
+                       {
+                           NSMutableDictionary *mailDictionary = [NSMutableDictionary dictionaryWithDictionary:email];
+                           mailDictionary[@"attachments"] = response;
+                           [safeMe sendEmailToPath:@"" parameters:email completion:block];
+                       }
+                   }];
         
-    } else {
-        [_client setHeaders:@{@"Authorization": self.kzToken,
-                              @"Accept" :@"application/json"}];
-        [_client POST:@"" parameters:email completion:^(id response, NSHTTPURLResponse *urlResponse, NSError *error) {
-            NSError * restError = nil;
-            if ([urlResponse statusCode]>KZHttpErrorStatusCode) {
-                restError = error;
-            }
-            block( [[KZResponse alloc] initWithResponse:response urlResponse:urlResponse andError:restError] );
-        }];
     }
+    else
+    {
+        [self sendEmailToPath:@"" parameters:email completion:block];
+    }
+}
+
+- (void) sendEmailToPath:(NSString *)path
+              parameters:(NSDictionary *)parameters
+              completion:(void (^)(KZResponse *r))block
+{
+    [_client setHeaders:@{@"Authorization": self.kzToken,
+                          @"Accept" :@"application/json"}];
+    
+    [_client POST:path parameters:parameters completion:^(id response, NSHTTPURLResponse *urlResponse, NSError *error) {
+        NSError * restError = nil;
+        if ([urlResponse statusCode]>KZHttpErrorStatusCode) {
+            restError = error;
+        }
+        block( [[KZResponse alloc] initWithResponse:response urlResponse:urlResponse andError:restError] );
+    }];
+
+    
 }
 
 @end
