a0206448536d6d14d10f967735827fe5186260c6
Nicolas Miyasato <nmiyasato@gmail.com>
2014-06-09 15:50:31 -0300

    Adding webView for passive authentication

---
+++ kidozen.client/kidozen.client/KZPassiveAuthViewController.h
@@ -0,0 +1,13 @@
+//
+//  KZPassiveAuthViewController.h
+//  kidozen.client
+//
+//  Created by Nicolas Miyasato on 6/9/14.
+//  Copyright (c) 2014 Tellago Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KZPassiveAuthViewController : UIViewController
+
+@end
