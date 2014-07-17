426eb1e5f9c178603461610cacc431e2584e4ef6
Nicolas Miyasato <nmiyasato@gmail.com>
2014-07-17 12:21:42 -0300

    Removing unused cache

--- kidozen.client/kidozen.client/KZApplicationServices.h
+++ kidozen.client/kidozen.client/KZApplicationServices.h
@@ -70,7 +70,4 @@
 
 - (KZNotification *)pushNotifications;
 
-
-
-
 @end
--- kidozen.client/kidozen.client/KZApplicationServices.m
+++ kidozen.client/kidozen.client/KZApplicationServices.m
@@ -28,15 +28,6 @@
 @property (nonatomic, strong) KZApplicationConfiguration *applicationConfig;
 @property (nonatomic, strong) KZTokenController *tokenController;
 @property (nonatomic, assign) BOOL strictSSL;
-
-@property (nonatomic, strong) NSMutableDictionary *queues;
-@property (nonatomic, strong) NSMutableDictionary *configurations;
-@property (nonatomic, strong) NSMutableDictionary *storages;
-@property (nonatomic, strong) NSMutableDictionary *smssenders;
-@property (nonatomic, strong) NSMutableDictionary *channels;
-@property (nonatomic, strong) NSMutableDictionary *services;
-@property (nonatomic, strong) NSMutableDictionary *datasources;
-
 @property (strong, nonatomic) KZLogging *log;
 @property (strong, nonatomic) KZMail *mail;
 @property (strong, nonatomic) KZNotification *pushNotifications;
@@ -55,14 +46,6 @@
         self.strictSSL = strictSSL;
         self.tokenController = tokenController;
         
-        self.queues = [[NSMutableDictionary alloc] init];
-        self.services = [[NSMutableDictionary alloc] init];
-        self.storages = [[NSMutableDictionary alloc] init];
-        self.configurations = [[NSMutableDictionary alloc] init];
-        self.smssenders = [[NSMutableDictionary alloc] init];
-        self.datasources = [[NSMutableDictionary alloc] init];
-        self.channels = [[NSMutableDictionary alloc] init];
-        
         [self initializeLogging];
         [self initializeMail];
         [self initializePushNotifications];
@@ -73,33 +56,24 @@
 
 - (KZQueue *)QueueWithName:(NSString *)name
 {
-    NSAssert(self.queues, @"Should have already a queues dictionary");
-    
     KZQueue * q = [[KZQueue alloc] initWithEndpoint:self.applicationConfig.queue
                                             andName:name];
     q.tokenController = self.tokenController;
     [q setBypassSSL:self.strictSSL];
-    [self.queues setObject:q forKey:name];
     return q;
 }
 
 - (KZStorage *)StorageWithName:(NSString *)name
 {
-    NSAssert(self.storages, @"Should have already a storages dictionary");
-    
     NSString * ep = [self.applicationConfig.storage stringByAppendingString:@"/"];
     KZStorage * s= [[KZStorage alloc] initWithEndpoint:ep andName:name];
     s.tokenController = self.tokenController;
     [s setBypassSSL:self.strictSSL];
-    [self.storages setObject:s forKey:name];
     return s;
 }
 
 - (KZService *)LOBServiceWithName:(NSString *)name
 {
-    if (!self.services) {
-        self.services = [[NSMutableDictionary alloc] init];
-    }
     //url: "/api/services/" + name + "/invoke/" + method,
     NSString *ep = [self.applicationConfig.url stringByAppendingString:
                     [NSString stringWithFormat:@"api/services/%@/",name]];
@@ -108,62 +82,44 @@
     s.tokenController = self.tokenController;
     [s setBypassSSL:self.strictSSL];
     
-    [self.services setObject:s forKey:name];
-    
     return s;
 }
 
 - (KZConfiguration *)ConfigurationWithName:(NSString *)name
 {
-    NSAssert(self.configurations, @"Should have already a configurations dictionary");
-    
     KZConfiguration * c = [[KZConfiguration alloc] initWithEndpoint:self.applicationConfig.config
                                                             andName:name];
     c.tokenController = self.tokenController;
     [c setBypassSSL:self.strictSSL];
-    [self.configurations setObject:c forKey:name];
     return c;
 }
 
 -(KZSMSSender *) SMSSenderWithNumber:(NSString *) number
 {
-
-    NSAssert(self.smssenders, @"Should have already a smsSenders dictionary");
-    
     KZSMSSender *s = [[KZSMSSender alloc] initWithEndpoint:self.applicationConfig.sms
                                                 andName:number];
     s.tokenController = self.tokenController;
     [s setBypassSSL:self.strictSSL];
-    [self.smssenders setObject:s forKey:number];
     return s;
 }
 
 -(KZDatasource *) DataSourceWithName:(NSString *)name
 {
-    if (!self.datasources) {
-        self.datasources = [[NSMutableDictionary alloc] init];
-    }
     NSString * ep = [self.applicationConfig.datasource stringByAppendingString:@"/"];
     
     KZDatasource * s= [[KZDatasource alloc] initWithEndpoint:ep andName:name];
     s.tokenController = self.tokenController;
     [s setBypassSSL:self.strictSSL];
-    
-    [self.datasources setObject:s forKey:name];
-    
     return s;
 }
 
 -(KZPubSubChannel *) PubSubChannelWithName:(NSString *) name
 {
-    NSAssert(self.channels, @"Should have already a channels dictionary");
-    
     KZPubSubChannel * ch =[[KZPubSubChannel alloc] initWithEndpoint:self.applicationConfig.pubsub
                                                          wsEndpoint:self.applicationConfig.ws
                                                             andName:name];
     ch.tokenController = self.tokenController;
     [ch setBypassSSL:self.strictSSL];
-    [self.channels setObject:ch forKey:name];
     return ch;
 }
 
