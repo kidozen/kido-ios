363e433d7dbdc605f943bd07d0dcdf3d28f6cdce
Nicolas Miyasato <nmiyasato@gmail.com>
2014-07-22 12:31:22 -0300

    Removing unused libraries

--- lib/1.0.1/include/KZLOBService.h
+++
@@ -1,8 +0,0 @@
-
-#import "KZBaseService.h"
-
-@interface KZLOBService : KZBaseService
-
--(void) invokeMethod:(NSString *) method withData:(id)data completion:(void (^)(KZResponse *))block;
-
-@end
