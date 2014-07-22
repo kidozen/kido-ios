2090bbf5c904ce921f597d187f5d2330c0eef4ca
Nicolas Miyasato <nmiyasato@gmail.com>
2014-07-22 13:26:15 -0300

    Updating 1.2 included library

---
+++ lib/1.2/fat/include/KZApplicationServices.h
@@ -0,0 +1,73 @@
+//
+//  KZApplicationServices.h
+//  kidozen.client
+//
+//  Created by Nicolas Miyasato on 6/19/14.
+//  Copyright (c) 2014 Tellago Studios. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "KZLogging.h"
+
+@class KZQueue;
+@class KZStorage;
+@class KZService;
+@class KZConfiguration;
+@class KZSMSSender;
+@class KZDatasource;
+@class KZPubSubChannel;
+@class KZApplicationConfiguration;
+@class KZTokenController;
+@class KZResponse;
+@class KZMail;
+@class KZNotification;
+
+/*
+ * The main idea of this class is to handle everything related to Logging,
+ * Email, notifications, SMS, etc...
+ */
+@interface KZApplicationServices : NSObject
+
+- (instancetype)initWithApplicationConfig:(KZApplicationConfiguration *)applicationConfig
+                          tokenController:(KZTokenController *)tokenController
+                                strictSSL:(BOOL)strictSSL;
+
+- (KZQueue *)QueueWithName:(NSString *)name;
+- (KZStorage *)StorageWithName:(NSString *)name;
+- (KZService *)LOBServiceWithName:(NSString *)name;
+- (KZConfiguration *)ConfigurationWithName:(NSString *)name;
+- (KZSMSSender *)SMSSenderWithNumber:(NSString *)number;
+- (KZDatasource *)DataSourceWithName:(NSString *)name;
+
+#if TARGET_OS_IPHONE
+- (KZPubSubChannel *)PubSubChannelWithName:(NSString *) name;
+#endif
+
+#pragma mark - Logging
+
+- (void)write:(id)object message:(NSString *)message withLevel:(LogLevel)level completion:(void (^)(KZResponse *))block;
+- (void)clearLog:(void (^)(KZResponse *))block;
+- (void)allLogMessages:(void (^)(KZResponse *))block;
+- (KZLogging *)log;
+
+
+
+#pragma mark - Email
+
+-(void) sendMailTo:(NSString *)to
+              from:(NSString *)from
+       withSubject:(NSString *)subject
+       andHtmlBody:(NSString *)htmlBody
+       andTextBody:(NSString *)textBody
+       attachments:(NSDictionary *)attachments
+        completion:(void (^)(KZResponse *))block;
+
+- (KZMail *)mail;
+
+
+
+#pragma mark - PushNotifications
+
+- (KZNotification *)pushNotifications;
+
+@end
--- lib/pre-release/fat/include/KZIdentityProviderFactory.h
+++
@@ -1,6 +0,0 @@
-#import <Foundation/Foundation.h>
-#import "KZIdentityProvider.h"
-
-@interface KZIdentityProviderFactory : NSObject
-+(id<KZIdentityProvider>) createProvider:(NSString *) type bypassSSL:(BOOL) bypassSSL;
-@end
