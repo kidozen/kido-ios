823564a30f05281868d0bb44ea0aea74301a6471
Nicolas Miyasato <nmiyasato@gmail.com>
2014-09-05 11:21:25 -0300

    Adding parameter naem

--- kidozen.client/kidozen.client/KZApplicationAuthentication.h
+++ kidozen.client/kidozen.client/KZApplicationAuthentication.h
@@ -48,7 +48,7 @@
 /**
  * Starts a passive authentication flow.
  */
-- (void)doPassiveAuthenticationWithCompletion:(void (^)(id))block;
+- (void)doPassiveAuthenticationWithCompletion:(void (^)(id a))block;
 
 // Refreshes the current token, which can be the one obtained from authenticathing
 // via username/password, passive authentication or via application key.
