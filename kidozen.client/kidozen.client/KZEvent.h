4c2f100fcf6136b5f45965e1964a10684850cb02
Nicolas Miyasato <nmiyasato@gmail.com>
2014-11-18 16:39:06 -0300

    Commeting some code.

--- kidozen.client/kidozen.client/KZEvent.h
+++ kidozen.client/kidozen.client/KZEvent.h
@@ -6,13 +6,26 @@
 //  Copyright (c) 2014 Kidozen. All rights reserved.
 //
 
+/**
+ *  The Event base class. Contains the barely minimum properties that an event needs.
+ */
 @interface KZEvent : NSObject
+
+/**
+ *  Initializer.
+ *
+ *  @param eventName   Is the name of this event. An example can be something like "Click, View, etc..."
+ *  @param sessionUUID The session ID to which this event belongs.
+ *
+ */
+-(instancetype) initWithEventName:(NSString *)eventName sessionUUID:(NSString *)sessionUUID;
 
 @property (nonatomic, readonly, copy) NSString *eventName;
 @property (nonatomic, readonly, copy) NSString *sessionUUID;
 
--(instancetype) initWithEventName:(NSString *)eventName sessionUUID:(NSString *)sessionUUID;
-
+/**
+ *  @return a dictionary representation of this event
+ */
 - (NSDictionary *)serializedEvent;
 
 @end
--- kidozen.client/kidozen.client/KZEvents.h
+++ kidozen.client/kidozen.client/KZEvents.h
@@ -10,15 +10,40 @@
 
 @class KZEvent;
 
+/**
+ *  Is a handy class that contains all events.
+ */
 @interface KZEvents : NSObject
 
+/**
+ *  Creates an instance of KZEvents with events that are persisted on disk.
+ *  If there are no events in disk, then we just create an empty instance.
+ *
+ */
 +(instancetype)eventsFromDisk;
 
 @property (nonatomic, readonly) NSMutableArray *events;
 
+/**
+ *  Will remove any persisted events
+ */
 - (void)removeSavedEvents;
+
+/**
+ *  Adds and event to the session.
+ *
+ *  @param event is the event that will be added.
+ */
 - (void)addEvent:(KZEvent *)event;
+
+/**
+ *  Saves current session with all current events.
+ */
 - (void)save;
+
+/**
+ *  Will remove current in-memory events.
+ */
 - (void)removeCurrentEvents;
 
 @end
