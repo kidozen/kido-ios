87cf9dea4892798f73c63ab363ddd880b0b0e945
Nicolas Miyasato <nmiyasato@gmail.com>
2015-02-17 22:14:03 -0300

    Showing token.

--- sampleApps/GoodSample/GoodSample/ViewController.m
+++ sampleApps/GoodSample/GoodSample/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "AppDelegate.h"
+#import <KZApplication.h>
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextView *textView;
 
 @end
 
@@ -24,6 +27,19 @@
     // Dispose of any resources that can be recreated.
 }
 - (IBAction)getToken:(id)sender {
+    KZApplication *kzApplication = [AppDelegate sharedDelegate].kzApplication;
+    __weak ViewController *safeMe = self;
+    
+    [kzApplication.gtDelegate getGTToken:@"challenge"
+                                  server:@"goodcontrol.kidozen.com"
+                                 success:^(NSString *token) {
+                                     NSLog(@"THE TOKEN IS %@", token);
+                                     safeMe.textView.text = token;
+        
+                                 } error:^(NSError *error) {
+                                     NSLog(@"There was an error dude... %@", error);
+                                     safeMe.textView.text = error.localizedDescription;
+                                 }];
     
 }
 
